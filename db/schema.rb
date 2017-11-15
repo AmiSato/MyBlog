db9aa526e7fcbeb4bac096e80e0fa2793ddb8ffb
Ami Sato <mitarasidangoda@gmail.com>
2017-11-15 11:25:39 +0000

    new-column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114080026) do
+ActiveRecord::Schema.define(version: 20171115112401) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171114080026) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "age"
   end
 
 end
