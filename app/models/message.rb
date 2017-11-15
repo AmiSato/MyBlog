b864e0fa23542d8234e0ed57023cf5c5151e0aba
Ami Sato <mitarasidangoda@gmail.com>
2017-11-15 11:49:52 +0000

    Add validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
 validates :title , length: { maximum: 32 } , presence: true
 validates :message , length: { maximum: 256 } , presence: true
 validates :name , length: { maximum: 32 } 
+validates :age , numericality: { only_integer: true, greater_than: 0, less_than: 100}
 end
